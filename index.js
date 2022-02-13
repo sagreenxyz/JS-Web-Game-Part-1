b600bde2e4fd53cbc6ccc530da38c4c798d0ac9c
Scott Green <sagreenxyz@gmail.com>
2022-02-12 20:33:55 -0600

    5b - create newItem() function

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function newImage(imgFile, left, bottom) {
     return image;
 }
 
+function newItem(itemFile, left, bottom) {
+    let item = newImage(itemFile, left, bottom);
+    item.addEventListener('click', () => {
+        item.remove();
+    });
+}
+
 newImage('assets/green-character.gif', 100, 100);
 newImage('assets/pine-tree.png', 450, 200);
 
@@ -17,7 +24,4 @@ newImage('assets/pillar.png', 350, 100);
 newImage('assets/crate.png', 150, 200);
 newImage('assets/well.png', 500, 425);
 
-let sword = newImage('assets/sword.png', 500, 405);
-sword.addEventListener('click', () => {
-    sword.remove();
-})
+newItem('assets/sword.png', 500, 405);

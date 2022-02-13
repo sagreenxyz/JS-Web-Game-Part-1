c757cb1deb2ce2ab6d0f9131d04aa5d8b9d06eda
Scott Green <sagreenxyz@gmail.com>
2022-02-12 20:31:09 -0600

    5 - add an item

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function newImage(imgFile, left, bottom) {
-    let image = document.createElement('img')
-    image.src = imgFile
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
-    document.body.append(image)
+    let image = document.createElement('img');
+    image.src = imgFile;
+    image.style.position = 'fixed';
+    image.style.left = left + 'px';
+    image.style.bottom = bottom + 'px';
+    document.body.append(image);
+    return image;
 }
 
 newImage('assets/green-character.gif', 100, 100);
@@ -15,3 +16,8 @@ newImage('assets/tree.png', 200, 300);
 newImage('assets/pillar.png', 350, 100);
 newImage('assets/crate.png', 150, 200);
 newImage('assets/well.png', 500, 425);
+
+let sword = newImage('assets/sword.png', 500, 405);
+sword.addEventListener('click', () => {
+    sword.remove();
+})

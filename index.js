718a519765de87a76ee058a6afa031432fcc4c0c
Scott Green <sagreenxyz@gmail.com>
2022-02-12 20:27:07 -0600

    3 - refactor using a function

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgFile, left, bottom) {
+    let greenCharacter = document.createElement('img')
+    greenCharacter.src = imgFile
+    greenCharacter.style.position = 'fixed'
+    greenCharacter.style.left = left + 'px'
+    greenCharacter.style.bottom = bottom + 'px'
+    document.body.append(greenCharacter)
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+newImage('assets/green-character.gif', 100, 100);
+newImage('assets/pine-tree.png', 450, 200);
